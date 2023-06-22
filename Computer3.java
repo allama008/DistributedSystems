7bd2ba559080a5d730aa8bea4ba2e880e7f22017
allama008 <a2hossain2000@yahoo.com>
2023-06-22 12:27:50 -0500

    Completed the function establishConnection() and client logic.

--- Computer3.java
+++ Computer3.java
@@ -6,8 +6,10 @@
     3.2 Append the information into the file with file name provided by the user
  */
 
+import java.io.BufferedInputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -16,18 +18,42 @@ public class Computer3
     private static DataInputStream inputStream = null;
     private static DataOutputStream outputStream = null;
 
-    private static void establishConnection(int serverPorts[], String serverIP)
+    private static String requestFile()
     {
-        try(Socket socket = new Socket(serverIP, serverPorts[0]))
+        
+        return "";
+    }
+
+    private static String establishConnection(int serverPorts, String serverIP, String fileName)
+    {
+        String transferStatus = "";
+        try(Socket socket = new Socket(serverIP, serverPorts))
         {
-            inputStream = new DataInputStream(socket.getInputStream());
+            inputStream = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
             outputStream = new DataOutputStream(socket.getOutputStream());
-            
+            outputStream.writeUTF(fileName);
+            String receivedMsg = "";
+            try
+            {
+                receivedMsg = inputStream.readUTF();
+            }
+            catch(IOException ioErrMsg)
+            {
+                System.out.println(ioErrMsg);
+            }
+            if(receivedMsg == "YES")
+                transferStatus = requestFile();
+            else
+                transferStatus = "NO";
+            socket.close();
+            inputStream.close();
+            outputStream.close();
         }
         catch(Exception e)
         {
             e.printStackTrace();
         }
+        return transferStatus;
     }
 
     public static void main(String[] args)
@@ -35,9 +61,19 @@ public class Computer3
         Scanner inputObj = new Scanner(System.in);
         System.out.println("Kindly input a file name: ");
         String inputFileName = inputObj.nextLine();
+        inputObj.close();
 
         String serverIPAddresses = new String("127.0.0.1");
         int serverPortNumber[] = new int[] {1600, 1601};
+
+        String status = establishConnection(serverPortNumber[0], serverIPAddresses, inputFileName);
+        if(status == "NO")
+            status = establishConnection(serverPortNumber[1], serverIPAddresses, inputFileName);
+        
+        if(status == "NO")
+            System.out.println("File transfer failure");
+        
+        System.out.println(status);
 
     }
 }
